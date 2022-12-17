50393a62afe69aa1b0577bf0f7c4cbdc5694b173
Debjyoti Roy <debjyotiroy039@gmail.com>
2022-12-17 20:57:40 +0530

    Add files via upload

---
+++ GL/src/com/department/AdminDepartment.java
@@ -0,0 +1,13 @@
+package com.department;
+
+public class AdminDepartment extends SuperDepartment{
+    public String departmentName(){
+        return "Welcome to Admin Department" ;
+
+    }
+    public String getTodaysWork(){
+        return "Complete your documents Submission" ;
+
+    }
+
+}
---
+++ GL/src/com/department/HrDepartment.java
@@ -0,0 +1,20 @@
+package com.department;
+
+public class HrDepartment extends SuperDepartment{
+    public String departmentName(){
+        return "Welcome to Hr Department" ;
+
+    }
+    public String getTodaysWork(){
+        return "Fill today's timesheet and mark your attendance" ;
+
+    }
+    public String getWorkDeadline(){
+        return "Complete by Eod" ;
+
+    }
+    public String doActivity(){
+        return "team Lunch" ;
+
+    }
+}
